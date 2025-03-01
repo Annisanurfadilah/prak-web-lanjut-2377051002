02d87d692e9aa817e37201501b44db050d91db31
Annisanurfadilah <143465026+Annisanurfadilah@users.noreply.github.com>
2025-03-01 21:54:57 +0700

    Menambah Profile

---
+++ database/migrations/2025_02_24_050916_create_mahasiswa_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('mahasiswa', function (Blueprint $table) {
+            $table->id();
+            $table->integer('mahasiswa_id')->unique();
+            $table->foreign('mahasiswa_id')->references('id')->non('mahasiswa')->onDalate('cascode');
+            $table->timestamps(); 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('mahasiswa');
+    }
+};
